6a3e4355e8893b9356da7932326caddec7653c57
wu-art <550681795@qq.com>
2021-02-02 14:44:36 +0800

    y头部样式调整

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,5 +1,25 @@
 <template>
-  <div>home</div>
+<el-container class="container">
+    <el-header class="header">
+        <el-row>
+            <el-col :span="4">
+                <img src="../../assets/pictures/login.jpg" alt="图片无法显示" class="login-img">
+            </el-col>
+            <el-col :span="18" class="login-middle">
+                <h3>基于web的旅游景点网站的设计与实现</h3>
+            </el-col>
+            <el-col :span="2">
+                <div class="grid-content bg-purple">
+                    <a href="#" class="loginout">退出</a>
+                </div>
+            </el-col>
+        </el-row>
+    </el-header>
+    <el-container>
+        <el-aside class="aside" width="200px">Aside</el-aside>
+        <el-main class="main">Main</el-main>
+    </el-container>
+</el-container>
 </template>
 
 <script>
@@ -9,5 +29,32 @@ export default {
 </script>
 
 <style>
+.container {
+    height: 100%;
+}
 
+.header {
+    background-color: #B3C0D1;
+}
+
+.aside {
+    background-color: #D3DCE6;
+}
+
+.main {
+    background-color: #E9EEF3;
+}
+/* 头部样式 */
+.login-img{
+    height: 60px;
+    width: 180px;
+}
+.login-middle{
+    /* line-height: 50px; */
+    text-align: center;
+}
+.loginout{
+    text-decoration: none;
+    line-height: 60px;
+}
 </style>
